54021c9957ad098bcd3c05eed74da9d3d5b2f8b2
Jonay Godoy <jonay.godoy.reyes@gmail.com>
2022-02-17 21:03:05 +0000

    template is done

--- src/main/java/marketplace/queries/home/HomeController.java
+++ src/main/java/marketplace/queries/home/HomeController.java
@@ -3,16 +3,44 @@ package marketplace.queries.home;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String home() {
-        return "marketplace/home/index";
+    public ModelAndView home(Model model) {
+        var pizzas = Arrays.asList(
+                new PizzaDto("bbq.png","bbq", 3.99f),
+                new PizzaDto("4Cheeses.png","4Cheeses", 4.99f),
+                new PizzaDto("carbonara.png","carbonara", 2.99f),
+                new PizzaDto("Cesar.png","Cesar", 5.99f),
+                new PizzaDto("hawaiana.PNG","hawaiana", -3.99f),
+                new PizzaDto("nachos.png","nachos", -30.99f)
+        );
+        return new ModelAndView(
+                "marketplace/home/index",
+                "pizzas",
+                pizzas);
+    }
+
+    public class PizzaDto {
+        public String img;
+        public String name;
+        public String price;
+
+        public PizzaDto(String img, String name, float price) {
+            this.img = img;
+            this.name = name;
+            this.price = price+"€";
+        }
     }
 
 }
