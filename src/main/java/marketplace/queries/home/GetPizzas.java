f660af768a6d80048c80835e01fca1a74b5ad479
Jonay Godoy <jonay.godoy.reyes@gmail.com>
2022-02-18 10:48:40 +0000

    ignore: clean

--- src/main/java/marketplace/queries/home/GetPizzas.java
+++ src/main/java/marketplace/queries/home/GetPizzas.java
@@ -3,7 +3,6 @@ package marketplace.queries.home;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.Arrays;
 import java.util.List;
 
 @Component("GetPizzas")
