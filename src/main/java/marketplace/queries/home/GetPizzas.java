c7070cde801317fe7bbea28b093bc57200c72eb4
Jonay Godoy <jonay.godoy.reyes@gmail.com>
2022-02-18 10:48:04 +0000

    add PizzaRepository

--- src/main/java/marketplace/queries/home/GetPizzas.java
+++ src/main/java/marketplace/queries/home/GetPizzas.java
@@ -1,5 +1,6 @@
 package marketplace.queries.home;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
@@ -8,15 +9,14 @@ import java.util.List;
 @Component("GetPizzas")
 public class GetPizzas {
 
-    public List<Pizza> Execute(){
-        return Arrays.asList(
-                new Pizza("bbq.png","bbq", 3.99f),
-                new Pizza("4Cheeses.png","4Cheeses", 4.99f),
-                new Pizza("carbonara.png","carbonara", 2.99f),
-                new Pizza("Cesar.png","Cesar", 5.99f),
-                new Pizza("hawaiana.PNG","hawaiana", -3.99f),
-                new Pizza("nachos.png","nachos", -30.99f)
-        );
+    private final PizzaRepository repository;
+
+    @Autowired
+    public GetPizzas(PizzaRepository repository) {
+        this.repository = repository;
     }
 
+    public List<Pizza> Execute(){
+        return repository.getAll();
+    }
 }
--- src/main/java/marketplace/queries/home/PizzaRepository.java
+++ src/main/java/marketplace/queries/home/PizzaRepository.java
@@ -2,6 +2,7 @@ package marketplace.queries.home;
 
 import org.springframework.stereotype.Repository;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Repository
@@ -9,6 +10,13 @@ public class PizzaRepository implements IPizzaRepository{
 
     @Override
     public List<Pizza> getAll() {
-        throw new UnsupportedOperationException();
+        return Arrays.asList(
+                new Pizza("bbq.png","bbq", 3.99f),
+                new Pizza("4Cheeses.png","4Cheeses", 4.99f),
+                new Pizza("carbonara.png","carbonara", 2.99f),
+                new Pizza("Cesar.png","Cesar", 5.99f),
+                new Pizza("hawaiana.PNG","hawaiana", -3.99f),
+                new Pizza("nachos.png","nachos", -30.99f)
+        );
     }
 }
