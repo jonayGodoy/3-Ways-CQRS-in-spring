893dd8186b0c18728f7c711823af303dcb66d42a
Jonay Godoy <jonay.godoy.reyes@gmail.com>
2022-02-21 14:47:16 +0000

    rabbitmq is working

---
+++ Handlers/src/test/java/com/handlers/handlers/HandlersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.handlers.handlers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandlersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
