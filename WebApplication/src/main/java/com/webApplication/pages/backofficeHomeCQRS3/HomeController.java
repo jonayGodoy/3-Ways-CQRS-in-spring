64f9f8e15ea70a332d953761dc104138ec689a8e
Jonay Godoy <jonaydam2@gmail.com>
2022-03-21 12:10:38 +0000

    #refactor: move to folder

---
+++ WebApplication/src/main/java/com/webApplication/pages/backofficeHomeCQRS3/HomeController.java
@@ -0,0 +1,18 @@
+package com.webApplication.pages.backofficeHomeCQRS3;
+
+import domain.backoffice.createPizzaCQRS3.command.AddPizzaRequestDto;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller("BackofficeHomeController3")
+public class HomeController {
+
+    @GetMapping("/backoffice3")
+    public ModelAndView home() {
+        return new ModelAndView(
+                "backoffice/homeCQRS3/index",
+                "addPizzaRequestDto",
+                new AddPizzaRequestDto());
+    }
+}
