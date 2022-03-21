94d4e4eddf2b0ec54fd68ca1a7cb2b19df38d9c9
Jonay Godoy <jonaydam2@gmail.com>
2022-03-21 12:10:22 +0000

    backoffice homeController cqrs4 works

---
+++ WebApplication/src/main/java/com/webApplication/pages/backofficeHomeCQRS4/AddPizzaRequestDto.java
@@ -0,0 +1,32 @@
+package com.webApplication.pages.backofficeHomeCQRS4;
+
+public class AddPizzaRequestDto {
+    private String img;
+    private String name;
+    private String price;
+
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+}
